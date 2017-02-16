bfc804ea7389a9400642e946b4bc7f03b805dbe4
aaschmei.17 <aaschmei.17@CHS-RMLAB213-22.CHUFSD.Org>
2017-02-16 08:49:01 -0500

    file delete

--- ReplayTest/src/data/PlayerData.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-public interface PlayerData {
-	public static final double PLAYER_SIZE = 1;
-	public static final double PLAYER_ACCELERATION = 0.25d/Data.UPS, PLAYER_DECCELERATION = 0.8d;
-	public static final double PLAYER_SPEED_LIMIT = 0.4d, PLAYER_ACCELERATION_LIMIT = 0.1d*Data.UPS;
-}
