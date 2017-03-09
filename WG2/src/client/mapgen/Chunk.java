51ae9e622f5981d9a4444ac61942c72ed98f8358
404Errr <auggieam@gmail.com>
2017-03-09 13:55:32 -0500

    seam

--- WG2/src/client/mapgen/Chunk.java
+++ WG2/src/client/mapgen/Chunk.java
@@ -6,20 +6,24 @@ import util.Util;
 
 public class Chunk implements Data, LayoutGenData {
 	private int[][] layout;
+	private int[][] seams;
 	
 	public Chunk(int[][] layout) {
 		this.layout = layout;
+		setSeams(layout);
 		Util.printIntAsCharArray(getSeams(layout));
 	}
 	
-	private static int[][] getSeams(int[][] layout) {
-		int[][] seams = new int[4][CHUNK_SIZE];
+	private void setSeams(int[][] layout) {
 		for (int i = 0;i<4;i++) {
 			seams[i] = Util.getArraySlice(layout, i);
 		}
-		return seams;
 	}
 
+	public int[][] getSeam(int side) {
+		return seams[side];
+	}
+	
 	public int[][] getLayout() {
 		return layout;
 	}
