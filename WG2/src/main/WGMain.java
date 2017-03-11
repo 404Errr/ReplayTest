85fe39698bac596840e42d6e285d901d63aa5abf
404Errr <auggieam@gmail.com>
2017-03-11 16:53:38 -0500

    dumb deletion

--- WG2/src/main/WGMain.java
+++ WG2/src/main/WGMain.java
@@ -4,16 +4,6 @@ import client.main.Client;
 
 public class WGMain {
 	public static void main(String[] args) {
-
-//		int[][] foo = Util.getRandomArray(10, 5, 10);
-//
-//		Util.printArray(foo);
-//		System.out.println("\n\n");
-//		Util.dumbThing(foo, true);
-//
-//		Util.printArray(foo);
-
-
 		Client.run();
 
 	}
